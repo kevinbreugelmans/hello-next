6b8983afe9b5e4f08c52473d95fba48398c11d9a
Kevin <kevin.breugelmans@gmail.com>
2018-03-05 23:13:04 +0100

    links and styles applied

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Header from '../components/Header'
+export default () => ([
+    <Header />,
+    <div>
+        <p>the about page</p>
+    </div>
+])
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-const Index = () => (
+import Link from 'next/link'
+import Header from '../components/Header'
+const Index = () => ([
+    <Header />,
     <div>
+        <Link href="/about" style={{fontSize: 20}}>
+            <button>about page</button>
+        </Link>
         <p>Hello world</p>
     </div>
-)
+])
 
 export default Index
